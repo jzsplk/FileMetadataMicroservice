fe1c40e9493b21567683a69aeb725684d7ac58ec
System <none>
2017-07-24 08:32:49 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-app.listen(process.env.PORT || 3000, )
+app.post('/upload', upload.single('file'), ())
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("everything is working")
+});
 
 
