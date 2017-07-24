f051b627d7f534623ebebb2b59f915b9901c937e
System <none>
2017-07-24 08:21:49 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -2,37 +2,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
+var multer = require('multer');
 
 var app = module.exports = express();
 app.use(bodyParser.json());
-app.use(cors());
-//get call to return JSON that format natural and unix data
-app.get('/dateValues/:dateVal', function(req, res, next){
-    console.log('url works');
-    var dateVal = req.params.dateVal;
-    var dataFormattingOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    };
-    
-    if(isNaN(dateVal)){
-        var naturalDate = new Date(dateVal);
-        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
-        var unixDate = new Date(dateVal).getTime()/1000;
-    }
-    else{
-        var unixDate = dateVal;
-        var naturalDate = new Date(dateVal *1000);
-        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions);
-    }
-    res.json({unix: unixDate, natural: naturalDate});
-});
+app.use(cors())
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + "/views/index.html")
-})
+app.listen(process.env.PORT || 3000, )
 
-app.listen(3000, function(){
-    console.log("It's  working");
-});
+
